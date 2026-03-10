cac3548b45431172aab13bafbf4dd8eea10e1ede
MD Sakibur Rahman <103588995+msrofficial@users.noreply.github.com>
2026-03-10 09:10:39 +0600

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,10 +1,7 @@
 export default function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method not allowed' });
-  }
+  if (req.method !== 'POST') return res.status(405).send('Method Not Allowed');
 
   const { password } = req.body;
-  
   if (password === process.env.ADMIN_PASS) {
     res.status(200).json({ success: true });
   } else {
